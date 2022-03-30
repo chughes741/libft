de15f68c93c6d12eff52709fad411b15c347b71f
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-03-30 19:28:47 -0400

    Moved around functions

--- tests/unittests_libft.c
+++ tests/unittests_libft.c
@@ -6,17 +6,17 @@ int	main() {
 /*			Setup			*/
 
 /*			atoi			*/
-	// Invalid inputs
-	char str1[32] = "aaa123";
-	int_expect_eq("atoi - invalid input 1", atoi(str1), ft_atoi(str1));
-	char str2[32] = " 123";
-	int_expect_eq("atoi - invalid input 2", atoi(str2), ft_atoi(str2));
-
 	// Valid input
 	char str3[32] = "321535234";
-	int_expect_eq("atoi - valid input 1", atoi(str3), ft_atoi(str3));
+	int_expect_eq("atoi - VI 1", atoi(str3), ft_atoi(str3));
 	char str4[32] = "-321535234";
-	int_expect_eq("atoi - valid input 2", atoi(str4), ft_atoi(str4));
+	int_expect_eq("atoi - VI 2", atoi(str4), ft_atoi(str4));
+
+	// Invalid inputs
+	char str1[32] = "aaa123";
+	int_expect_eq("atoi - NVI 1", atoi(str1), ft_atoi(str1));
+	char str2[32] = " 123";
+	int_expect_eq("atoi - NVI 2", atoi(str2), ft_atoi(str2));
 
 
 /*			bzero			*/
@@ -40,19 +40,35 @@ int	main() {
 /*			isalnum			*/
 	// Valid inputs
 	int num1 = 48;
-	int_expect_eq("isalnum - valid input 1", isalnum(num1), ft_isalnum(num1));
+	int_expect_eq("isalnum - VI 1", isalnum(num1), ft_isalnum(num1));
 	int num2 = 122;
-	int_expect_eq("isalnum - valid input 2", isalnum(num2), ft_isalnum(num2));
+	int_expect_eq("isalnum - VI 2", isalnum(num2), ft_isalnum(num2));
 
 	// Invalid inputs
 	int num3 = -12;
-	int_expect_eq("isalnum - invalid input 1", isalnum(num3), ft_isalnum(num3));
+	int_expect_eq("isalnum - NVI 1", isalnum(num3), ft_isalnum(num3));
 	int num4 = 200;
-	int_expect_eq("isalnum - invalid input 2", isalnum(num4), ft_isalnum(num3));
+	int_expect_eq("isalnum - NVI 2", isalnum(num4), ft_isalnum(num3));
 
 /*			isalpha			*/
+	// Valid inputs
+	char str9[32] = "asdgASDASD";
+	int_expect_eq("isalpha - VI 1", isalpha(str9), ft_isalpha(str9));
+
+	// Invalid inputs
+	char str10[32] = " asd";
+	int_expect_eq("isalpha - NVI 1", isalpha(str10), ft_isalpha(str10));
+
 /*			isascii			*/
 /*			isdigit			*/
+	// Valid inputs
+	char str11[32] = "123124";
+	int_expect_eq("isdigit - VI 1", isdigit(str11), ft_isdigit(str11));
+
+	// Invalid inputs
+	char str12[32] = "123124a";
+	int_expect_eq("isdigit - NVI 1", isdigit(str12), ft_isdigit(str12));
+
 /*			isprint			*/
 /*			itoa			*/
 /*			memchr			*/
