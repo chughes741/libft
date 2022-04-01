d8e30059660fced9a5f6347b4f214e384b1fb7e1
Cole - MBA <coltenhughes@gmail.com>
2022-03-31 22:12:25 -0400

    Updated: variable names

--- libft.h
+++ libft.h
@@ -38,7 +38,7 @@ char	*ft_itoa(int n);
 void	*ft_memchr(const void *str, int c, size_t n);
 int		ft_memcmp(const void *str1, const void *str2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *str1, const void *str2, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *str, int c, size_t n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
