9fef6a2dcae3d1d3dd0042fdfb9a972b35521125
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-01 18:07:41 -0400

    Finished: passing all tests

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,8 +14,14 @@
 
 int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
-	(void) *str1;
-	(void) *str2;
-	(void) n;
+	int	i;
+
+	i = 0;
+	while (n--)
+	{
+		if (str1[i] != str2[i])
+			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+		i++;
+	}
 	return (0);
 }
