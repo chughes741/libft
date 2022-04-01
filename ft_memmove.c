915c3402dc859e071992832967f54710e16c6885
Cole - MBA <coltenhughes@gmail.com>
2022-03-31 22:20:06 -0400

    Finished: passing tests

--- ft_memmove.c
+++ ft_memmove.c
@@ -15,9 +15,27 @@
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t	i;
+	char	*destc;
+	char	*srcc;
+	char	*temp;
 
-	(void) *dest;
-	(void) *src;
-	(void) n;
-	return (0);
+	i = 0;
+	if (dest == NULL || src == NULL)
+		return (NULL);
+	temp = (char *)malloc(sizeof(char) * n);
+	destc = (char *)dest;
+	srcc = (char *)src;
+	while (i < n) 
+	{
+		temp[i] = srcc[i];
+		i++;
+	}
+	i = 0;
+	while (i < n)
+	{
+		destc[i] = temp[i];
+		i++;
+	}
+	free(temp);
+	return (dest);
 }
