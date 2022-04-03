5b62f3d4e657cb9ce6545c7480efda86ae35a500
Cole - MBA <coltenhughes@gmail.com>
2022-04-02 20:42:41 -0400

    Finished: passing all tests

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -14,10 +14,10 @@
 
 char	*ft_strrchr(const char *str, int c)
 {
-	int	i;
+	size_t	i;
 
 	i = (ft_strlen(str));
-	while (i <= 0)
+	while (i > 0)
 	{
 		if (str[i] == c)
 			return ((char *)&str[i]);
