d0f97a6fc63f0a7106f4cdcddb1e190b147ffd2d
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-04 15:28:50 -0400

    First draft, not passing one test on wm

--- ft_substr.c
+++ ft_substr.c
@@ -14,8 +14,16 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	(void) *s;
-	(void) start;
-	(void) len;
-	return (0);
+	char		*str;
+	const char	*first;
+
+	if (start > ft_strlen(s))
+		return (NULL);
+	first = &s[start];
+	str = malloc((len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	ft_strlcpy(str, first, (len + 1));
+	str[len] = '\0';
+	return (str);
 }
