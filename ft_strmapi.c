4856b982bd9b21f4e22d8004dbd0fb89138bfc96
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-04 15:06:28 -0400

    First prototype

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -14,7 +14,17 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	(void) *s;
-	(void) *f;
-	return (0);
+	unsigned int	i;
+	char			*str;
+
+	i = 0;
+	str = malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	return (str);
 }
