395c787b28efae149087c451d79bfc1ac68e309e
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-05 13:02:43 -0400

    First proto, passing most tests

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,9 +12,34 @@
 
 #include "libft.h"
 
+int	ft_setmatch(char const *c, char const *set)
+{
+	int	i;
+
+	i = 0;
+	if (c == NULL || set == NULL)
+		return (0);
+	while (set[i])
+	{
+		if (set[i] == *c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	(void) *s1;
-	(void) *set;
-	return (0);
+	char			*rtn;
+	unsigned int	start;
+	size_t			len;
+
+	start = 0;
+	len = ft_strlen(s1);
+	while (ft_setmatch(&s1[start], set) == 1)
+		start++;
+	while (ft_setmatch(&s1[len - 1], set) == 1)
+		len--;
+	rtn = ft_substr(s1, start, (len - start));
+	return (rtn);
 }
