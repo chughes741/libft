3b7f552419831c82d7b5356d75d0a6534d79a970
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-05 16:58:55 -0400

    Logic in place, timing out

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -14,6 +14,12 @@
 
 int	ft_lstsize(t_list *lst)
 {
-	(void) *lst;
-	return (0);
+	int	i;
+
+	if (lst == NULL)
+		return (0);
+	i = 1;
+	while (lst->next != NULL)
+		i += ft_lstsize(lst->next);
+	return (i);
 }
