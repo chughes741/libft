477981f3f97e63869e05ec5a02ed8616aa751fa4
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-05 15:16:16 -0400

    Finished, can't get tests working

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -14,6 +14,10 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	(void) *content;
-	return (0);
+	t_list	*new_list;
+
+	new_list = malloc(sizeof(t_list));
+	new_list->content = content;
+	new_list->next = NULL;
+	return (new_list);
 }
