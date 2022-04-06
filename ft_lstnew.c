f7498d4dc2dfff414a07e7d2053d7ec8cdc64da1
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-06 14:01:24 -0400

    Modified to use calloc

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*new_list;
 
-	new_list = malloc(sizeof(t_list));
+	new_list = ft_calloc(1, sizeof(t_list));
 	new_list->content = content;
 	new_list->next = NULL;
 	return (new_list);
