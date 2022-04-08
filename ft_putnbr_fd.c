222f0c148778708b05c4040c033027bec04a92f2
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-08 13:58:40 -0400

    Refactored to use recursion

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putnbr_fd.nbr                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chughes <marvin@42quebec.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -14,11 +14,26 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*c;
-	size_t	len;
+	char nbr;
 
-	c = ft_itoa(n);
-	len = ft_strlen(c) * sizeof(char);
-	write(fd, &c[0], len);
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = -n;
+		ft_putnbr_fd(n, fd);
+	}
+	else if (n < 10)
+	{
+		nbr = n + '0';
+		write(fd, &nbr, 1);
+	}
+	else
+	{
+		ft_putnbr_fd(n / 10, fd);
+		nbr = (n % 10) + '0';
+		write(fd, &nbr, 1);
+	}
 	return ;
 }
