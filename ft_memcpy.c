44390e862398fe5515332f815a56a16f90260bd6
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-10 13:35:17 -0400

    Fixed: consistent naming

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -12,21 +12,21 @@
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
 	char	*str1;
 	char	*str2;
 
 	i = 0;
-	if (dest == NULL && src == NULL)
+	if (dst == NULL && src == NULL)
 		return (NULL);
-	str1 = (char *)dest;
+	str1 = (char *)dst;
 	str2 = (char *)src;
 	while (i < n)
 	{
 		str1[i] = str2[i];
 		i++;
 	}
-	return (dest);
+	return (dst);
 }
