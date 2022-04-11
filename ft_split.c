0e2c7fe31df0bbd3813a3ee302c5bf0336bd0ced
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-11 15:53:02 -0400

    Still a few bugs to sort out

--- ft_split.c
+++ ft_split.c
@@ -14,50 +14,57 @@
 
 size_t	ft_wordcount(char const *s, char c)
 {
-	size_t	rtn;
-	int		j;
+	size_t	count;
+	size_t	len;
+	size_t	i;
 
-	rtn = 0;
-	j = 0;
-	while (s[j])
+	count = 0;
+	i = 0;
+	len = ft_strlen(s);
+	while (i < len)
 	{
-		while (s[j] == c && s[j] != '\0')
-			j++;
-		if (s[j] != '\0')
-			rtn++;
-		while (s[j] != c)
-			j++;
+		while (s[i] == c)
+			i++;
+		if (s[i] != c && s[i] != '\0')
+			count++;
+		while (s[i] != c)
+			i++;
 	}
-	return (rtn);
+	printf("Word count: %zu\n", count);
+	return (count);
 }
 
 char	*ft_nextword(char const *s, char c)
 {
 	size_t	len;
+	size_t	slen;
 	char	*rtn;
 
 	len = 0;
-	while (s[len] && s[len] != c)
+	slen = ft_strlen(s);
+	while (s[len] != c && len < slen)
 		len++;
 	rtn = (char *)calloc(len + 1, sizeof(char));
 	if (rtn == NULL)
 		return (NULL);
-	strlcpy(rtn, s, len);
+	ft_strlcpy(rtn, s, len + 1);
 	return (rtn);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**rtn;
-	int		i;
-	int		j;
+	size_t	len;
+	size_t	i;
+	size_t	j;
 
 	rtn = (char **)calloc(ft_wordcount(s, c) + 1, sizeof(char *));
 	if (rtn == NULL)
 		return (NULL);
 	i = 0;
 	j = 0;
-	while (s[i])
+	len = ft_strlen(s);
+	while (i < len)
 	{
 		while (s[i] == c)
 			i++;
@@ -66,6 +73,17 @@ char	**ft_split(char const *s, char c)
 		while (s[i] != c)
 			i++;
 	}
-	rtn[j] = (void *)0;
+	rtn[ft_wordcount(s, c) + 1] = (void *)0;
 	return (rtn);
+}
+
+int main() {
+	char *s1 = "     split     this for   me  !    ";
+	char c = ' ';
+	char **s2 = ft_split(s1, c);
+	int i = 0;
+
+	while (s2[i]) {
+		printf("%s\n", s2[i]);
+		i++;}
 }
