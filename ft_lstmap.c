7a734b51fbc5436fb39f9df9ef98eea5acc2ea3d
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-12 13:17:16 -0400

    Removed comments

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -12,11 +12,6 @@
 
 #include "libft.h"
 
-/* Iterates the list ’lst’ and applies the function ’f’ on the content of
-each node. Creates a new list resulting of the successive applications of
-the function ’f’.  The ’del’ function is used to delete the content of
-a node if needed. */
-
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*newlist;
