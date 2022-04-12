98856ab94420fa44f16019f4c12915eedabfdc0b
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-12 13:17:50 -0400

    Added checks

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -16,7 +16,15 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
-	last = ft_lstlast(*lst);
-	last->next = new;
+	if (new)
+	{
+		if (*lst)
+		{
+			last = ft_lstlast(*lst);
+			last->next = new;
+			return ;
+		}
+		*lst = new;
+	}
 	return ;
 }
