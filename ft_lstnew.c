541cc64116f2d45e07c9419f94f4b0cc966edbcb
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-18 14:16:26 -0400

    Added: alloc prot

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -17,6 +17,8 @@ t_list	*ft_lstnew(void *content)
 	t_list	*new_list;
 
 	new_list = ft_calloc(1, sizeof(t_list));
+	if (new_list == NULL)
+		return (NULL);
 	new_list->content = content;
 	new_list->next = NULL;
 	return (new_list);
